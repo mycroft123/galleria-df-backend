fdcad617c4aa087dfc2772fea527d438874dbcec
mycroft123 <mycroft55@hotmail.com>
2024-12-27 02:57:15 -0500

    Remove node_modules from the repository

--- node_modules/@web3-storage/blob-index/dist/test/sharded-dag-index.spec.d.ts
+++
@@ -1,4 +0,0 @@
-export const test: {
-    'from to archive': (assert: typeof import("entail/assert")) => Promise<void>;
-};
-//# sourceMappingURL=sharded-dag-index.spec.d.ts.map
--- node_modules/@web3-storage/w3up-client/dist/test/capability/store.test.d.ts
+++
@@ -1,3 +0,0 @@
-export const StoreClient: Test.Suite<undefined>;
-import * as Test from '../test.js';
-//# sourceMappingURL=store.test.d.ts.map
